d46e95a7e464710cb23702092c1d240d5d997a8b
Carlos G. Fernandez <ionlotus1993@gmail.com>
2024-02-22 03:38:42 -0500

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -34,7 +34,7 @@ function calculatePoints() {
         if (currentScore < goal) {
             document.getElementById('result').innerHTML = "Remaining promoters to reach goal: " + Math.round(Math.abs(remainingPoints));
         } else if (currentScore > goal) {
-            document.getElementById('result').innerHTML = "Goal achieved! Exceeded by " + Math.round(currentScore - goal).style.color = 'green' + " Points";
+            document.getElementById('result').innerHTML = "Goal achieved! Exceeded by " + Math.round(currentScore - goal) + " Points";
         } else if (currentScore == goal) {
             document.getElementById('result').innerHTML = "Goal has been achieved! ";
         }  else {
