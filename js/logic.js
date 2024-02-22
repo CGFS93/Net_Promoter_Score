178b6f7ce51313a8d642c42b14a3adbe34ad9752
Carlos G. Fernandez <ionlotus1993@gmail.com>
2024-02-22 05:10:32 -0500

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -13,7 +13,7 @@ function calculatePoints() {
 
         var totalSurvey = promoter + neutral + detractor;
         var currentScore = ((promoter - detractor) / totalSurvey) * 100;
-        var remainingPoints = (goal * totalSurvey) / 100;
+        var remainingPoints = (goal * totalSurvey + detractor) / 100;
 
         // Update the score display
         document.getElementById('score').innerHTML = "Current Score: " + currentScore;
